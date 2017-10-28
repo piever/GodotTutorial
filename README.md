66029be4b763b4929fd5baa822761d3f00658d40
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-10-28 19:46:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# A Simple Shooting Task
+
+Godot Tutorial for the Students Retreat.
+Images and assets are from http://kenney.nl and [dyogurt](https://twitter.com/dfmmatias).
---
+++ scripts/player.gd
@@ -0,0 +1,30 @@
+extends Sprite
+
+# class member variables go here, for example:
+
+const BASELINE_SPEED = 200
+const SPEED = 400
+const ANGULAR_SPEED = 2
+var signed_speed = 0
+var dir = 0
+onready var top_left = get_node("../background/top_left").get_global_pos()
+onready var bottom_right = get_node("../background/bottom_right").get_global_pos()
+
+func _ready():
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	if Input.is_action_pressed("ui_up"):
+		signed_speed = SPEED
+	elif Input.is_action_pressed("ui_down"):
+		signed_speed = -SPEED
+	else:
+		signed_speed = 0
+	if Input.is_action_pressed("ui_left"):
+		dir += delta*ANGULAR_SPEED
+	if Input.is_action_pressed("ui_right"):
+		dir -= delta*ANGULAR_SPEED
+	translate(delta*(signed_speed+BASELINE_SPEED)*Vector2(cos(dir), -sin(dir)))
+	set_rot(dir-PI/2)
+
+
