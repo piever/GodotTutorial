95d3bed165d14af67b8afe586a9b14d49f99c524
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-10-31 12:06:47 +0000

    pedagogical version

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,22 +1,24 @@
 extends Sprite
 
 # class member variables go here, for example:
+var angle
 var dir
-var SPEED = 800
+const SPEED = 800
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	translate(delta*SPEED*Vector2(cos(dir), -sin(dir)))
-	set_rot(dir-PI/2)
+	dir = Vector2(cos(angle), -sin(angle))
+	translate(delta*SPEED*dir)
+	set_rot(angle-PI/2)
 
-func _on_visible_exit_screen():
-	queue_free()
+#func _on_visible_exit_screen():
+#	queue_free()
 
 
-func _on_area_area_enter( area ):
-	if area.is_in_group("enemies"):
-		area.get_parent().queue_free()
-		queue_free()
-		print(get_tree().get_nodes_in_group("enemies").size())
+#func _on_area_area_enter( area ):
+#	if area.is_in_group("enemies"):
+#		area.get_parent().queue_free()
+#		queue_free()
+#		print(get_tree().get_nodes_in_group("enemies").size())
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,37 +2,40 @@ extends Sprite
 
 # class member variables go here, for example:
 
-const BASELINE_SPEED = 200
-const SPEED = 400
-const ANGULAR_SPEED = 2
-var signed_speed = 0
-var dir = 0
+var dir = Vector2(0, -1)
+var speed = 200
+#const BASELINE_SPEED = 200
+#const EXTRA_SPEED = 400
+#const ANGULAR_SPEED = 2
+#var angle = 0
+
 #onready var top_left = get_node("../background/top_left").get_global_pos()
 #onready var bottom_right = get_node("../background/bottom_right").get_global_pos()
 
 func _ready():
 	set_fixed_process(true)
-	set_process_input(true)
+#	set_process_input(true)
 
-func _input(event):
-	if event.is_action_pressed("shoot"):
-		var bullet = load("res://scenes/bullet.tscn").instance()
-		bullet.dir = dir
-		bullet.set_pos(get_pos())
-		get_parent().add_child(bullet)
+#func _input(event):
+#	if event.is_action_pressed("shoot"):
+#		var bullet = load("res://scenes/bullet.tscn").instance()
+#		bullet.angle = angle
+#		bullet.set_pos(get_pos())
+#		get_parent().add_child(bullet)
 
 func _fixed_process(delta):
-	if Input.is_action_pressed("ui_up"):
-		signed_speed = SPEED
-	elif Input.is_action_pressed("ui_down"):
-		signed_speed = -SPEED
-	else:
-		signed_speed = 0
-	if Input.is_action_pressed("ui_left"):
-		dir += delta*ANGULAR_SPEED
-	if Input.is_action_pressed("ui_right"):
-		dir -= delta*ANGULAR_SPEED
-	translate(delta*(signed_speed+BASELINE_SPEED)*Vector2(cos(dir), -sin(dir)))
-	set_rot(dir-PI/2)
+#	if Input.is_action_pressed("ui_up"):
+#		speed = BASELINE_SPEED+EXTRA_SPEED
+#	elif Input.is_action_pressed("ui_down"):
+#		speed = BASELINE_SPEED-EXTRA_SPEED
+#	else:
+#		speed = BASELINE_SPEED
+#	if Input.is_action_pressed("ui_left"):
+#		angle += delta*ANGULAR_SPEED
+#	if Input.is_action_pressed("ui_right"):
+#		angle -= delta*ANGULAR_SPEED
+#	dir = Vector2(cos(angle), -sin(angle))
+	translate(delta*speed*dir)
+#	set_rot(angle-PI/2)
 
 
